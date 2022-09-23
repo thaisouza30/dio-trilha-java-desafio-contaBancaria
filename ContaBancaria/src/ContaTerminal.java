764eb8539d8bfb483b8a70cc3aeb9bb383161f00
thais.souza <thaisouzacruz@gmail.com>
2022-09-23 19:28:30 -0300

    Desafio Dio

---
+++ ContaBancaria/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Por favor, Digite seu primeiro nome:");
+        String nome = terminal.next();
+
+        System.out.println("Digite seu Sobrenome:");
+        String sobrenome = terminal.next();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = terminal.next();
+
+        System.out.println("Digite o número da conta:");
+        int numero = terminal.nextInt();
+
+        System.out.println("Digite o saldo inicial:");
+        Float saldo = terminal.nextFloat();
+
+        System.out.println("Olá " + nome +" "+ sobrenome +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta "+ numero +
+                " e seu saldo "+ saldo +" já está disponível para saque");
+
+        terminal.close();
+
+    }
+}
